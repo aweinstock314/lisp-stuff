7277fa3f3cdeb43fe7b3f64957d790218e4d192b
Avi Weinstock <aweinstock314@gmail.com>
2014-07-30 18:09:26 -0400

    Changed music_test to use a higher-level interface.

--- kawa_jogl/music_test/music_test.scm
+++ kawa_jogl/music_test/music_test.scm
@@ -3,17 +3,11 @@
 
 (define synth::javax.sound.midi.Synthesizer (javax.sound.midi.MidiSystem:getSynthesizer))
 (synth:open)
-(define rec::javax.sound.midi.Receiver (synth:getReceiver))
 
-(define-macro (sm x) `(static-field javax.sound.midi.ShortMessage ',x))
-(define-macro (make-note a b c)
-    (define-gensyms x)
-    `(returning (,x (javax.sound.midi.ShortMessage))
-        (invoke ,x 'setMessage ,a ,b ,c)
+(define-macro (with-channel synthesizer name num . body)
+    `(let ((,name :: javax.sound.midi.MidiChannel ((invoke ,synthesizer 'getChannels) ,num)))
+        ,@body
     )
-)
-(define-macro (emit a b c)
-   `(invoke rec 'send (make-note ,a ,b ,c) -1)
 )
 
 (define (sweep lo hi step)
@@ -24,16 +18,34 @@
     )
 )
 
-(define instr 000)
-(define pitch 127)
-(define pitchsweeper (sweep 30 70 4))
+(pascal-for (i 0 3 1) (future (begin
+    (define pitch 127)
+    (define pitchsweeper (sweep 30 70 4))
+    (define lastpitch 0)
+    (define instr (->int (truncate (random 127))))
+    (java.lang.Thread:sleep (* 5000 i))
+    (with-channel synth ch i
+        (with-min-ms-per-iteration (+ 500 (* 250 i))
+            (ch:noteOff lastpitch)
+            (printf "instrument %s; pitch %s\n" instr pitch)
+            (ch:programChange instr)
+            (synchronized pitch
+                (set! lastpitch (pitchsweeper pitch))
+                (set! pitch lastpitch)
+            )
+            ;(inc! instr 7)
+            ;(inplace! (wrap 0 127) instr)
+            (ch:noteOn lastpitch #x7f)
+        )
+    )
+)))
 
-(with-min-ms-per-iteration 250
-    (emit (sm NOTE_OFF) pitch 0)
-    (printf "instrument %s; pitch %s\n" instr pitch)
-    (emit (sm PROGRAM_CHANGE) instr 0)
-    (inplace! pitchsweeper pitch)
-    (inc! instr 7)
-    (inplace! (wrap 0 127) instr)
-    (emit (sm NOTE_ON) pitch #x7f)
+(define pitch2 00)
+(define sweeper2 (sweep 35 38 100))
+(with-channel synth ch 9
+    (with-min-ms-per-iteration 500
+        (ch:noteOff pitch2)
+        (inplace! sweeper2 pitch2)
+        (ch:noteOn pitch2 #x7f)
+    )
 )
