f3d0b005f07d7eadecae375f11beede57baff082
Avi Weinstock <aweinstock314@gmail.com>
2014-11-29 17:52:46 -0500

    Changed scheme_util_include.mk to copy the TCPKeyboard* classes generated by scheme_util_networking.

--- kawa_jogl/scheme_util/scheme_util_include.mk
+++ kawa_jogl/scheme_util/scheme_util_include.mk
@@ -6,4 +6,4 @@ scheme_util_math.class: $(SCHEME_UTIL_PATH)/scheme_util_math.scm
 	cp -t . $(SCHEME_UTIL_PATH)/scheme_util_math*.class
 scheme_util_networking.class: $(SCHEME_UTIL_PATH)/scheme_util_networking.scm
 	cd $(SCHEME_UTIL_PATH)/ && make scheme_util_networking.class
-	cp -t . $(SCHEME_UTIL_PATH)/scheme_util_networking*.class
+	cp -t . $(SCHEME_UTIL_PATH)/scheme_util_networking*.class $(SCHEME_UTIL_PATH)/TCPKeyboardReceiver.class $(SCHEME_UTIL_PATH)/TCPKeyboardSender.class
